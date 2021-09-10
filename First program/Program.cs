33d595fb3e9a09f700fcda66f6de661fc1bf18f7
Viacheslav <Viacheslav@DESKTOP-15QFFN4>
2021-09-10 14:00:21 +0300

    Добавьте файлы проекта.

---
+++ First program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int n;
+            n=Convert.ToInt32(Console.ReadLine());
+            Console.Write(n);
+        }
+    }
+}
